13809ae546a5de67181ce0f422f92792fa17ead7
Gustavo <gustavoigordev@gmail.com>
2025-09-01 01:05:49 -0300

    Adding new features

--- features/bdd/employees.feature
+++ features/bdd/employees.feature
@@ -14,4 +14,15 @@ Feature: Manage employees
         Given the user registers a new employee
         When the user submits the employee details
         Then the employee should be created successfully
+
+    @scenario_03
+    Scenario: Update employee information
+        Given the user updates an existing employees information
+        When the user modifies the employee details
+        Then the employee information should be updated successfully
     
+    @scenario_04
+    Scenario: Delete an employee
+        Given the user wants to delete an existing employee
+        When the user sends the id
+        Then the employee should be deleted successfully
--- features/step_definitions/employees_steps.rb
+++ features/step_definitions/employees_steps.rb
@@ -35,3 +35,47 @@ Then('the employee should be created successfully') do
     expect(@create_employee['data']['employee_salary']).to eql (50000)
     expect(@create_employee['data']['employee_age']).to eql (30)
 end
+
+
+Given('the user updates an existing employees information')do
+    @get_employee = HTTParty.get('https://dummy.restapiexample.com/api/v1/employees', :headers => {'Content-Type': 'application/json'})
+    @put_url = 'https://dummy.restapiexample.com/api/v1/update/' + @get_employee['data'][0]['id'].to_s
+end
+ 
+When('the user modifies the employee details') do
+    @update_employee = HTTParty.put(@put_url, :headers => { 'Content-Type': 'application/json' }, body: {
+        "employee_name": "Gas",
+        "employee_salary": 12030,
+        "employee_age": 21,
+        "profile_image": ""
+    }.to_json) 
+
+    puts(@update_employee)
+end
+
+Then('the employee information should be updated successfully') do
+    expect(@update_employee.code).to eql 200
+    expect(@update_employee.message).to eql 'OK'
+    expect(@update_employee['data']['employee_name']).to eql 'Gas'
+    expect(@update_employee["status"]).to eql 'success'
+    expect(@update_employee['data']['employee_salary']).to eql (12030)
+    expect(@update_employee['data']['employee_age']).to eql (21)
+end
+
+Given('the user wants to delete an existing employee') do
+    @get_employee = HTTParty.get('https://dummy.restapiexample.com/api/v1/employees', :headers => {'Content-Type': 'application/json'})
+    @delete_url = 'https://dummy.restapiexample.com/api/v1/delete/' + @get_employee['data'][0]['id'].to_s
+end
+
+When('the user sends the id') do
+    @delete_employee = HTTParty.delete(@delete_url, :headers => { 'Content-Type': 'application/json' })
+    puts(@delete_employee)
+end
+
+Then('the employee should be deleted successfully') do       
+    expect(@delete_employee.code).to eql 200
+    expect(@delete_employee.message).to eql 'OK'
+    expect(@delete_employee["status"]).to eql 'success'
+    expect(@delete_employee['data']).to eql '2'
+end
+
